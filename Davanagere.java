7a9f44ce7d18f65762a4c5b8eb997ff439676e61
Ramyapxowrkz <ramyap.xworkz@gmail.com>
2023-10-05 21:28:13 +0530

    3

---
+++ Davanagere.java
@@ -0,0 +1,15 @@
+class Davanagere{
+   public static void main(String[] args)
+   {
+   int num=12;
+   num+=1;  //num=num+1
+   System.out.println("sum of  the number "+num);
+   num-=1;
+   System.out.println("sub of number "+num);
+   num/=12;
+   System.out.println("Division of number "+num);
+   num*=12;
+   System.out.println("multiplication of number "+num);
+   
+   }
+   }
