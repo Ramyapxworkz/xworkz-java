b6a143dc2e6a490dd4b27783b022bac5a148913a
Ramyapxowrkz <ramyap.xworkz@gmail.com>
2023-10-05 21:30:09 +0530

    4

---
+++ Festival.java
@@ -0,0 +1,23 @@
+class Festival{
+  public static void festivalNmae(){
+    System.out.println("Ganesh Chathurthi");
+	}
+	 public static void dateOfCelebration(){
+    System.out.println("12/8");
+	}
+	 public static void food(){
+    System.out.println("Karjikaayi");
+	}
+	 public static void flowers(){
+    System.out.println("Different kinds of flowers like mallige so on");
+	}
+	 public static void festivalNmae1(){
+    System.out.println("Deepavali");
+	}
+	 public static void month(){
+    System.out.println("November");
+	}
+	 public static void food1(){
+    System.out.println("Holige");
+	}
+	}
