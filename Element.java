448033c7f4f449aa110ccf171fa2eedb1829e7eb
Ramyapxowrkz <ramyap.xworkz@gmail.com>
2023-10-11 18:22:42 +0530

    11

---
+++ Element.java
@@ -0,0 +1,26 @@
+package com.xworkz.september.things;
+
+public class Element {
+
+	public static void main(String[] args) {
+	   int[] name={1,2,4,4,5,7,9};
+	   String[] count1= {"ramya"};
+	   int count=count1.length;
+	   System.out.println(count);
+	   System.out.println(name.length);
+	   System.out.println(name[0]);
+	   System.out.println(name[1]);
+	   System.out.println(name[2]);
+	   System.out.println(name[3]);
+	   System.out.println(name[4]);
+	   System.out.println(name[5]);
+	   
+	   
+	   
+	   
+	   
+	   
+
+	}
+
+}
