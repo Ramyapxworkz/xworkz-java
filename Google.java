89b6a9739cf332a4a26483ff35eaae706489284c
Ramyapxowrkz <ramyap.xworkz@gmail.com>
2023-10-27 19:00:31 +0530

    12

---
+++ Google.java
@@ -0,0 +1,279 @@
+package com.xworkz.september.things;
+
+public class Google {
+	private String userName;
+	private long phoneNum;
+	private String password;
+	private String email;
+	private String app;
+	private String companyName;
+	private String headQuarters;
+	private String california;
+	private String ceo;
+	private String softwareDevelopment;
+	private int alphabetInc;
+	private String search;
+	private String platForm;
+	private int found;
+	private String com;
+	private String ads;
+	private String workSpace;
+	private String maps;
+	private String pay;
+	private String googlePhotos;
+	private String googleNews;
+	private String googleScholar;
+	private String googleEarth;
+	private String googleTranslate;
+	private String googleImage;
+	private String googleTrends;
+	private String googleAdsense;
+	private String googleAIEthics;
+	private String research;
+	private String innovation;
+	private String hardware;
+	private String developerSupport;
+	
+	private String engine;
+
+	public Google() {
+		System.out.println(".......................");
+	}
+	public void setUserName(String userName) {
+		this.userName=userName;
+	}
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	
+	public void setNumber(long phoneNum) {
+		this.phoneNum=phoneNum;
+	}
+	public long getNumber() {
+		return this.phoneNum;
+	}
+	
+	public void setPassword(String password) {
+		this.password=password;
+	}
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setEmail(String email) {
+		this.email=email;
+	}
+	public String getEmail() {
+		return this.email;
+	}
+	
+	
+	
+	public void setApp(String app) {
+		this.app=app;
+	}
+	
+	public String getApp() {
+		return this.app;
+	}
+	
+	public void setCompanyName(String companyName) {
+		this.companyName=companyName;
+	}
+	public String getCompanyName() {
+		return this.companyName;
+	}
+	
+	public void setHeadQuarters(String headQuarters) {
+		this.headQuarters=headQuarters;
+	}
+	public String getHeadQuarters() {
+		return this.headQuarters;
+	}
+	
+	public void setCalifornia(String california) {
+		this.california=california;
+	}
+	public String getCalifornia() {
+		return this.california;
+	}
+	
+	public void setCEO(String ceo) {
+		this.ceo=ceo;
+	}
+	public String getCEO() {
+		return this.ceo;
+	}
+	
+	public void setSoftwareDevelopment(String softwareDevelopment) {
+		this.softwareDevelopment=softwareDevelopment;
+	}
+	public String getSoftwareDevelopment() {
+		return this.softwareDevelopment;
+	}
+	
+	public void setAlphabetInc(int alphabetInc) {
+		this.alphabetInc=alphabetInc;
+	}
+	public int getAlphabetInc() {
+		return this.alphabetInc;
+	}
+	
+	public void setSearch(String search) {
+		this.search=search;
+	}
+	public String getSearch() {
+		return this.search;
+	}
+	
+	public void setPlatForm(String platForm) {
+		this.platForm=platForm;
+	}
+	public String getPlatForm() {
+		return this.platForm;
+	}
+	
+	public void setFound(int found) {
+		this.found=found;
+	}
+	public int getFound() {
+		return this.found;
+	}
+	
+	public void setCOM(String com) {
+		this.com=com;
+	}
+	public String getCOM() {
+		return this.com;
+	}
+	
+	public void setAds(String ads) {
+		this.ads=ads;
+	}
+	public String getAds() {
+		return this.ads;
+	}
+	
+	public void setWorkSpace(String workSpace) {
+		this.workSpace=workSpace;
+	}
+	public String getWorkSpac() {
+		return this.workSpace;
+	}
+	
+	public void setMaps(String maps) {
+		this.maps=maps;
+	}
+	public String getmaps() {
+		return this.maps;
+	}
+	
+	public void setPay(String pay) {
+		this.pay=pay;
+	}
+	public String getPay() {
+		return this.pay;
+	}
+	
+	public void setPhotos(String googlePhotos) {
+		this.googlePhotos=googlePhotos;
+	}
+	public String getPhotos() {
+		return this.googlePhotos;
+	}
+	
+	public void setNews(String googleNews) {
+		this.googleNews=googleNews;
+	}
+	public String getNews() {
+		return this.googleNews;
+	}
+	
+	public void setScholar(String googleScholar) {
+		this.googleScholar=googleScholar;
+	}
+	public String getScholar() {
+		return this.googleScholar;
+	}
+	
+	public void setEarth(String googleEarth) {
+		this.googleEarth=googleEarth;
+	}
+	public String getEarth() {
+		return this.googleEarth;
+	}
+	
+	public void setTranslate(String googleTranslate) {
+		this.googleTranslate=googleTranslate;
+	}
+	public String getTranslate() {
+		return this.googleTranslate;
+	}
+	
+	public void setImage(String googleImage) {
+		this.googleImage=googleImage;
+	}
+	public String getImage() {
+		return this.googleImage;
+	}
+	
+	public void setTrends(String googleTrends) {
+		this.googleTrends=googleTrends;
+	}
+	public String getTrends() {
+		return this.googleTrends;
+	}
+	
+	public void setAdsense(String googleAdsense) {
+		this.googleAdsense=googleAdsense;
+	}
+	public String getAdsense() {
+		return this.googleAdsense;
+	}
+	
+	public void setAIEthics(String googleAIEthics) {
+		this.googleAIEthics=googleAIEthics;
+	}
+	public String getAIEthics() {
+		return this.googleAIEthics;
+	}
+	
+	public void setResearch(String research) {
+		this.research=research;
+	}
+	public String getResearch() {
+		return this.research;
+	}
+	
+	public void setInnovation(String innovation) {
+		this.innovation=innovation;
+	}
+	public String getInnovation() {
+		return this.innovation;
+	}
+	
+	public void setHardware(String hardware) {
+		this.hardware=hardware;
+	}
+	public String getHardware() {
+		return this.hardware;
+	}
+	
+	public void setSupport(String developerSupport) {
+		this.developerSupport=developerSupport;
+	}
+	public String getSupport() {
+		return this.developerSupport;
+	}
+	
+	public void setEngine(String engine) {
+		this.engine=engine;
+	}
+	public String getEngine() {
+		return this.engine;
+	}
+	
+	
+
+}
---
+++ GoogleRunner.java
@@ -0,0 +1,141 @@
+package com.xworkz.september.driver;
+
+import com.xworkz.september.things.Google;
+
+public class GoogleRunner {
+    public static void main(String[] args) {
+	Google google=new Google();
+	google.setUserName("tyty");
+	String user=google.getUserName();
+	System.out.println(user);
+	
+	google.setNumber(87878787);
+	long num=google.getNumber();
+	System.out.println(num);
+	
+	google.setPassword("ramyap@123");
+	String passWord=google.getPassword();
+	System.out.println(passWord);
+	
+	google.setEmail("rramyap@gmail.com");
+	String email=google.getEmail();
+	System.out.println(email);
+	
+	
+	google.setApp("Google");
+	String app=google.getApp();
+	System.out.println(app);
+	
+	google.setCompanyName("Alphabet Inc");
+	String companyName=google.getCompanyName();
+	System.out.println(companyName);
+	
+	google.setHeadQuarters("Mountain View");
+	String headQuarters=google.getHeadQuarters();
+	System.out.println(headQuarters);
+	
+	google.setCalifornia("HeadQuarters");
+	String california=google.getCalifornia();
+	System.out.println(california);
+	
+	google.setCEO("Sundar Pichai");
+	String ceo=google.getCEO();
+	System.out.println(ceo);
+	
+	google.setSoftwareDevelopment("Android Operating System");
+	String softwareDevelopment=google.getSoftwareDevelopment();
+	System.out.println(softwareDevelopment);
+	
+	google.setAlphabetInc(2015);
+	int alphabetInc=google.getAlphabetInc();
+	System.out.println(alphabetInc);
+	
+	google.setSearch("web dev");
+	String search=google.getSearch();
+	System.out.println(search);
+	
+	google.setPlatForm("Cloud platform");
+	String platForm=google.getPlatForm();
+	System.out.println(platForm);
+	
+	google.setFound(1998);
+	int found=google.getFound();
+	System.out.println(found);
+	
+	google.setCOM("main website");
+	String com=google.getCOM();
+	System.out.println(com);
+	
+	google.setAds("This property allows advertisers");
+	String ads=google.getAds();
+	System.out.println(ads);
+	
+	google.setWorkSpace("A suite of cloud-based productivity");
+	String workSpace=google.getWorkSpac();
+	System.out.println(workSpace);
+	
+	google.setMaps("A mapping and navigation service");
+	String maps=google.getmaps();
+	System.out.println(maps);
+	
+	google.setPay("The digital distribution platform ");
+	String pay=google.getPay();
+	System.out.println(pay);
+	
+	google.setPhotos("A photo and video storage service offering free ");
+	String photos=google.getPhotos();
+	System.out.println(photos);
+	
+	google.setNews("A news aggregation service that provides customized news  ");
+	String news=google.getNews();
+	System.out.println(news);
+	
+	google.setScholar("A freely accessible web search engine ");
+	String scholar=google.getScholar();
+	System.out.println(scholar);
+	
+	google.setEarth("A virtual globe, map, and geographical information program");
+	String earth=google.getEarth();
+	System.out.println(earth);
+	
+	google.setTranslate("A service that translates text and web pages between different languages.");
+	String translate=google.getTranslate();
+	System.out.println(translate);
+	
+	google.setImage("A search service specifically for images");
+	String image=google.getImage();
+	System.out.println(image);
+	
+	google.setTrends("A tool that shows the popularity of search terms over time");
+	String trends=google.getTrends();
+	System.out.println(trends);
+	
+	google.setAdsense("display Google ads on their websites.");
+	String adsense=google.getAdsense();
+	System.out.println(adsense);
+	
+	google.setAIEthics("Google is also involved in discussions and research related to AI ethics");
+	String aIEthics=google.getAIEthics();
+	System.out.println(aIEthics);
+	
+	google.setResearch("Google is also involved in discussions and research related to AI ethics");
+	String research=google.getResearch();
+	System.out.println(research);
+	
+	google.setInnovation("Google is also involved in discussions and research related to AI ethics");
+	String innovation=google.getInnovation();
+	System.out.println(innovation);
+	
+	google.setHardware("Google is also involved in discussions and research related to AI ethics");
+	String hardware=google.getHardware();
+	System.out.println(hardware);
+	
+	google.setSupport("Google is also involved in discussions and research related to AI ethics");
+	String developerSupport=google.getSupport();
+	System.out.println(developerSupport);
+	
+	google.setEngine("Google is also involved in discussions and research related to AI ethics");
+	String engine=google.getEngine();
+	System.out.println(engine);
+}
+}
